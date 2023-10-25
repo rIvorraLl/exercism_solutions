44cc2918629c66e2ec1681e510b0b4dadb87e6c0
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-25 12:51:55 +0200

    add: ./AnnalynsInfiltration

--- src/exercism/Twofer.java
+++ src/exercism/Twofer.java
@@ -1,5 +1,7 @@
 package exercism;
 
 public class Twofer {
-
+    public String twofer(String name) {
+	throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
 }
