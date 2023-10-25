d12b34f6748722a378d5eb236e5f856cad37d1ac
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-25 23:08:32 +0200

    add: ./ElonsToyCar

---
+++ src/exercism/ElonsToyCar.java
@@ -0,0 +1,29 @@
+package exercism;
+
+public class ElonsToyCar {
+
+    private int distance = 0;
+    private int battery = 100;
+
+    public static ElonsToyCar buy() {
+	return new ElonsToyCar();
+    }
+
+    public String distanceDisplay() {
+	return "Driven " + distance + " meters";
+    }
+
+    public String batteryDisplay() {
+	if (battery == 0) {
+	    return "Battery empty";
+	}
+	return "Battery at " + battery + "%";
+    }
+
+    public void drive() {
+	if (battery > 0) {
+	    this.distance += 20;
+	    this.battery--;
+	}
+    }
+}
