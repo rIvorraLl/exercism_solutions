a0a7700853c58503cbc426a4711f96ad8f546a34
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-27 21:52:48 +0200

    add: ./Gigasecond

---
+++ src/exercism/Gigasecond.java
@@ -0,0 +1,26 @@
+package exercism;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Gigasecond {
+    LocalDate ld = null;
+    LocalDateTime dt = null;
+
+    public Gigasecond(LocalDate moment) {
+	this.ld = moment;
+    }
+
+    public Gigasecond(LocalDateTime moment) {
+	this.dt = moment;
+    }
+
+    public LocalDateTime getDateTime() {
+	if (ld != null) {
+	    LocalDateTime ldt = ld.atStartOfDay();
+	    return ldt.plusSeconds(1000000000);
+	}
+	return dt.plusSeconds(1000000000);
+    }
+}
