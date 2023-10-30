6b362bc2c6c6e0d42b3624b59e3a0fcb0a3433fb
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-10-30 20:52:38 +0100

    add: ./Grains

---
+++ src/exercism/Grains.java
@@ -0,0 +1,24 @@
+package exercism;
+
+import java.math.BigInteger;
+import java.math.BigDecimal;
+
+class Grains {
+
+    BigInteger grainsOnSquare(final int square) throws IllegalArgumentException {
+	if (square < 1 || square > 64) {
+	    throw new IllegalArgumentException("square must be between 1 and 64");
+	}
+	BigInteger result = new BigInteger("2");
+	result = result.pow(square - 1);
+	return result;
+    }
+
+    BigInteger grainsOnBoard() {
+	BigInteger sum = new BigInteger("0");
+	for (int i = 1; i <= 64; i++) {
+	    sum = sum.add(grainsOnSquare(i));
+	}
+	return sum;
+    }
+}
