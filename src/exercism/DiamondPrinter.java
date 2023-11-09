47ed589e31f25444865128409ed2bbb41ea786f0
rIvorraLl <rivorrallodra@cifpfbmoll.eu>
2023-11-09 19:09:24 +0100

    add: ./DiamondPrinter

--- src/exercism/DiamondPrinter.java
+++ src/exercism/DiamondPrinter.java
@@ -1,50 +1,58 @@
 package exercism;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 class DiamondPrinter {
 
     List<String> printToList(char a) {
 	ArrayList<String> diamond = new ArrayList<String>();
-	String vowels = "ABCDEFGHIJKLMNOPQRSQUVWXYZ";
+	String vowels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	String row = "";
 	int vowelIndex = vowels.indexOf(a) + 1;
 	int len = 2 * vowelIndex - 1;
 	int l = len / 2;
+	int r = len / 2 + 1;
+	int charIndex = 0;
 	for (int i = 0; i < vowelIndex; i++) {
-	    int r = len / 2 + 1;
 	    boolean rDone = false;
 	    for (int j = 0; j < len; j++) {
 		if (j == l) {
-		    row += "" + vowels.charAt(j);
+		    row += vowels.charAt(charIndex);
 		    l--;
-		} else if (j != 0 && j == r && !rDone) {
-		    row += "" + vowels.charAt(j);
+		} else if (i != 0 && j == r && !rDone) {
+		    row += vowels.charAt(charIndex);
 		    r++;
 		    rDone = true;
 		} else {
-		    row += ".";
+		    row += " ";
 		}
 	    }
-	    rDone = false;
 	    diamond.add(row);
+	    charIndex++;
 	    row = "";
 	}
-//        ····A····
-//        ···B·B···
-//        ··C···C··
-//        ·D·····D·
-//        E·······E
-//        ·D·····D·
-//        ··C···C··
-//        ···B·B···
-//        ····A····
+	charIndex--;
+	l = len - len + 1;
+	r = len - 2;
+	for (int k = 0; k < vowelIndex - 1; k++) {
+	    boolean lDone = false;
+	    for (int x = 0; x < len; x++) {
+		if (!lDone && x == l) {
+		    row += vowels.charAt(charIndex - 1);
+		    l++;
+		    lDone = true;
+		} else if (k != vowelIndex - 2 && x == r) {
+		    row += vowels.charAt(charIndex - 1);
+		    r--;
+		} else {
+		    row += " ";
+		}
+	    }
+	    diamond.add(row);
+	    charIndex--;
+	    row = "";
+	}
 	return diamond;
-    }
-    public static void main(String[] args) {
-	DiamondPrinter dp = new DiamondPrinter();
-	System.out.println(dp.printToList('E'));
     }
 }
